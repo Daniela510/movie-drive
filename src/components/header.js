de2309a667bcec2c3ddf34fd99ea8ed83ed872c9
Daniela Damaschin <whateverellie@gmail.com>
2022-02-16 20:50:22 +0100

    Basic React set up

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+    <div className="logo">Logo</div>
+    <nav>
+      <ul className="nav">
+        <li className="nav__item"><a href="#land">Home</a></li>
+        <li className="nav__item"><a href="#three">Three</a></li>
+        <li className="nav__item"><a href="#seven">Seven</a></li>
+        <li className="nav__item"><a href="#four">Four</a></li>
+        <li className="nav__item"><a href="#form">More</a></li>
+      </ul>
+    </nav>
+  </header>
+  )
+}
+
+export default Header
